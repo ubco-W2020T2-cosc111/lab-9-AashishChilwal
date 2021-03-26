4c3484dae06942bc889f365d550003d8d6c66f0e
AashishChilwal <78070029+AashishChilwal@users.noreply.github.com>
2021-03-26 16:09:12 -0600

    Update Q2.java

--- Q2.java
+++ Q2.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Q2
+public class Main
 {
 	public static void main(String[] args) {
 	    Scanner inputs = new Scanner(System.in);
@@ -7,19 +7,24 @@ public class Q2
 		double side_1 = inputs.nextDouble();
 		double side_2 = inputs.nextDouble();
 		double side_3 = inputs.nextDouble();
+		isValid(side_1,side_2,side_3);
+		System.out.println(area(side_1,side_2,side_3)+"is the area of the triangle");
 	}
 	 public static boolean isValid(double side1, double side2, double side3)
      {
-         if (side1 + side2 > side3 && side2+side3>side1 && side1+side3>side2){
-            System.out.println("this");
+         if (side1 + side2 > side3 && side2+side3>side1 && side1+side3>side2)
             return true;
-         }
-         else{
-              System.out.println("this");
+         
+         else
               return false;
          }
-            
-         
-     }
-}
-
+    public static double area(double side1, double side2, double side3){
+        double area_t;
+        if (isValid(side1,side2,side3) == true ){
+            area_t = Math.sqrt(side1*(side1-side2)*(side1-side2)*(side1-side3));
+        }
+        else {
+        }
+        return area_t;
+    }
+    }
