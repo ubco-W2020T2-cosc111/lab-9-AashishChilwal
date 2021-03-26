62b4970cca12b4b3a44d6757cb32aab9f5007bc6
AashishChilwal <78070029+AashishChilwal@users.noreply.github.com>
2021-03-26 05:10:27 -0600

    Q1 solved

--- Q1.java
+++ Q1.java
@@ -1,1 +1,21 @@
-
+import java.util.Scanner;
+public class Q1
+{
+	public static void main(String[] args) {
+	    Scanner input = new Scanner(System.in);
+		System.out.print("Enter time in secs: ");
+		int secs = input.nextInt();
+		convertTime(secs);
+	}
+	 public static String convertTime(int totalSeconds)
+	 {
+	     String str1; 
+	     int mins,seconds,hour; 
+	     hour = totalSeconds/3600;
+	     mins = totalSeconds/60;
+	     seconds = totalSeconds%60;
+	     str1 = String.valueOf(hour) + String.valueOf(mins)+String.valueOf(seconds);
+	     System.out.println(hour+":"+mins+":"+seconds);
+	     return str1;
+	 }
+}
