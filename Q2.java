24be37079e5fbbf7848efb5c2cee58ab040b5f30
AashishChilwal <78070029+AashishChilwal@users.noreply.github.com>
2021-03-26 18:27:36 -0600

    Q3 solved

--- Q2.java
+++ Q2.java
@@ -7,8 +7,10 @@ public class Main
 		double side_1 = inputs.nextDouble();
 		double side_2 = inputs.nextDouble();
 		double side_3 = inputs.nextDouble();
-		isValid(side_1,side_2,side_3);
-		System.out.println(area(side_1,side_2,side_3)+"is the area of the triangle");
+		if (isValid(side_1,side_2,side_3) == true)
+		    System.out.printf("Area: %.3f",area(side_1,side_2,side_3));
+		else    
+		    System.out.println("Invalid input. Try again.");
 	}
 	 public static boolean isValid(double side1, double side2, double side3)
      {
@@ -21,10 +23,12 @@ public class Main
     public static double area(double side1, double side2, double side3){
         double area_t;
         if (isValid(side1,side2,side3) == true ){
-            area_t = Math.sqrt(side1*(side1-side2)*(side1-side2)*(side1-side3));
+            double s = (side1 + side2 + side3) / 2;
+            return Math.sqrt(s*(s-side1)*(s-side2)*(s-side3));
         }
         else {
+            return 0.0;
         }
-        return area_t;
+       
     }
     }
